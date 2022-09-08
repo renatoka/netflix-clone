9e335c0d16aea2c60e3a9c080822c2fa7c6ec2cc
Renato <rkauric@vvg.hr>
2022-09-08 11:56:41 +0200

    Minor changes

--- src/main.js
+++ src/main.js
@@ -2,11 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBQpo_FF9wy_VCg6U5AdhA3gsd_UEslzGQ",
   authDomain: "netflix-clone-e9eae.firebaseapp.com",
