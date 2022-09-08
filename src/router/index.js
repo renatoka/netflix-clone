7013a3d22f6b1348bf1aaf2172fadc199e08ecf9
Renato <rkauric@vvg.hr>
2022-09-08 11:56:58 +0200

    Added route guarding

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory, routerKey } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import BrowseView from '../views/BrowseView.vue'
-
+import { getAuth } from 'firebase/auth'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,24 +13,31 @@ const router = createRouter({
     {
       path: '/browse',
       name: 'browse',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/BrowseView.vue'),
-      meta: {
-        requiresAuth: true
-      }
     },
     {
       path: '/login',
       name: 'login',
-      component: () => import('../views/LoginView.vue') 
+      component: () => import('../views/LoginView.vue')
     },
     {
       path: '/movies',
       name: 'movies',
-      component: () => import('../views/MoviesView.vue')
+      component: () => import('../views/MoviesView.vue'),
     }
   ]
 })
+
+/* Simple way of route guarding /browse and /movies page.
+If the user is not logged in, don't allow them to see those pages.
+Will update and add better security once I learn how */
+
+router.beforeEach((to) => {
+  if (to.path === '/browse' && getAuth().currentUser === null) {
+    router.push('/login')
+  } else if (to.path === '/movies' && getAuth().currentUser === null) {
+    router.push('/login')
+  }
+})
+
 export default router
